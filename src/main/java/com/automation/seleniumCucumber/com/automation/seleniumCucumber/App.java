cbd77a1d5d998de6adea02b02472200bfe8bab72
shobhit002 <shobhiteze@gmail.com>
2019-10-13 18:27:21 +0530

    First commit

---
+++ src/main/java/com/automation/seleniumCucumber/com/automation/seleniumCucumber/App.java
@@ -0,0 +1,13 @@
+package com.automation.seleniumCucumber.com.automation.seleniumCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
